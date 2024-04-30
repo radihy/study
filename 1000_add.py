27d0adcc37b13e88dcea2fccbc9b3658fbe180d9
radihy <soiyosauce@gmail.com>
2024-04-30 23:21:38 +0900

    first commit

---
+++ 1000_add.py
@@ -0,0 +1,17 @@
+# input1 = input()
+# output1 = int(input1[0])+int(input1[2])
+# print(output1)
+
+input1 = input()
+input2 = input()
+# print(type(input1))
+# output1 = int(input1[0])+int(input1[2])
+# output2 = sum(int(input1[0]), int(input1[2]))
+# output2= sum([1,2])
+
+list = [int(input1.split(' ')[0]), int(input2.split(' ')[0])]
+# print(list[0])
+# print(list[1])
+output2= sum(list)
+# print(output1)
+print(output2)
---
+++ 1000_add_ad.py
@@ -0,0 +1,8 @@
+
+input1 = input()
+
+num_list = [int(input1.split(' ')[0]), int(input1.split(' ')[1])]
+
+output2= sum(num_list)
+
+print(output2)
